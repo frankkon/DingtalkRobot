240272bcaa3a4af9d17cdd76f9273e0e6b0504bf
frank <frankkon@sina.com>
2020-08-31 19:50:45 +0800

    新增机器人的时候增加对机器人信息的校验； 去掉新增机器人界面展示当前机器人的表格，没必要了。

--- dialogrobot.cpp
+++ dialogrobot.cpp
@@ -3,6 +3,7 @@
 #include "mainwindow.h"
 
 #include <QMainWindow>
+#include <QMessageBox>
 
 DialogRobot::DialogRobot(QWidget *parent) :
     QDialog(parent),
@@ -18,6 +19,14 @@ DialogRobot::~DialogRobot()
 
 void DialogRobot::on_pushButton_clicked()
 {
+    if(ui->lineEdit->text().trimmed() == ""
+            || ui->lineEdit_2->text().trimmed() == ""
+            || ui->lineEdit_3->text().trimmed() == "")
+    {
+        QMessageBox::warning(this, "提示", "请输入正确的钉钉机器人信息！");
+        return;
+    }
+
     Robot* r = new Robot();
     r->setName(ui->lineEdit->text());
     r->setUrl(ui->lineEdit_2->text());
